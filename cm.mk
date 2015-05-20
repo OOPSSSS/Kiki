8275eb931bd2698e49ef7e9cce898deb34b6c666
OOPSSSS <belchenkovdenis@gmail.com>
2015-05-20 19:16:05 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,2 +1,10 @@
 # Kiki
 NX404H
+$(call inherit-product, device/ZteRed/V5.2.1/404.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_RELEASE_NAME := NX404H
+PRODUCT_DEVICE := 404
+PRODUCT_NAME := cm_404
